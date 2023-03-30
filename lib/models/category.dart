39d15d3c6106ca49bc67cd213914767d5cd9debf
admin <kimchi200401@gmail.com>
2023-03-30 15:24:27 +0700

    rest api

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,21 +1,18 @@
+class Category {
+  int? id;
+  String? name;
 
-class Category{
-  final int id;
-  final String name;
-  const Category({
-    required this.id,
-    required this.name,
-  });
-  factory Category.fromJson(Map<String, dynamic> json){
-    return Category(
-      id: json['id'],
-      name: json['name'],
-    );
+  Category({this.id, this.name});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
   }
+
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
     return data;
   }
 }
--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -1,3 +1,4 @@
+import 'package:petshop/models/category.dart';
 import 'package:petshop/models/tag.dart';
 
 class Pet {
@@ -48,21 +49,3 @@ class Pet {
   }
 }
 
-class Category {
-  int? id;
-  String? name;
-
-  Category({this.id, this.name});
-
-  Category.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    return data;
-  }
-}
